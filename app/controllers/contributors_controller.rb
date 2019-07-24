2bc8943fbbf928e2d441b09e0a10e295eba4aea0
codeshootah <grigorycode@gmail.com>
2019-07-24 16:52:45 +0300

    Parce url

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -1,6 +1,11 @@
 class ContributorsController < ApplicationController
   def index
-    url = 'https://api.github.com/repos/rails/rails/contributors'
-    @contributors = HTTParty.get(url)
+    if params[:q].blank?
+      @contributors = HTTParty.get("https://api.github.com/repos/rails/rails/contributors")
+    else
+      url = params[:q]
+      path = URI(url).path
+      @contributors = HTTParty.get("https://api.github.com/repos#{path}/contributors")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'contributors#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "contributors" => "contributors#index"
+  resources :contributors
 end
